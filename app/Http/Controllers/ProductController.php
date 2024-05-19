194f4fd5a1f89decf78d1cc252f71c5beea716e9
abhishek1cloud <161021671+abhishek1cloud@users.noreply.github.com>
2024-05-19 23:02:36 +0530

    Changes

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -69,19 +69,7 @@ class ProductController extends Controller
           return response()->json(['message' => 'Product deleted successfully!']);
       }
   
-    //   public function export($format)
-    //   {
-    //       $products = Product::with('variants')->get();
-  
-    //       if ($format === 'excel') {
-    //           return Excel::download(new ProductsExport($products), 'products.xlsx');
-    //       } elseif ($format === 'pdf') {
-    //           $pdf = PDF::loadView('products.pdf', compact('products'));
-    //           return $pdf->download('products.pdf');
-    //       }
-  
-    //       return redirect()->route('products.index');
-    //   }
+
     public function export($format)
     {
         $products = Product::with('variants')->get();
@@ -89,47 +77,35 @@ class ProductController extends Controller
         if ($format === 'excel') {
             return Excel::download(new ProductsExport($products), 'products.xlsx');
         } elseif ($format === 'pdf') {
-            // Create new PDF document
             $pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
 
-            // Set document information
             $pdf->SetCreator(PDF_CREATOR);
             $pdf->SetAuthor('Your Name');
             $pdf->SetTitle('Products');
             $pdf->SetSubject('Product List');
             $pdf->SetKeywords('TCPDF, PDF, products, export');
 
-            // Set default header data
             $pdf->SetHeaderData('', 0, 'Products', 'Product List');
 
-            // Set header and footer fonts
             $pdf->setHeaderFont([PDF_FONT_NAME_MAIN, '', PDF_FONT_SIZE_MAIN]);
             $pdf->setFooterFont([PDF_FONT_NAME_DATA, '', PDF_FONT_SIZE_DATA]);
 
-            // Set default monospaced font
             $pdf->SetDefaultMonospacedFont(PDF_FONT_MONOSPACED);
 
-            // Set margins
             $pdf->SetMargins(PDF_MARGIN_LEFT, PDF_MARGIN_TOP, PDF_MARGIN_RIGHT);
             $pdf->SetHeaderMargin(PDF_MARGIN_HEADER);
             $pdf->SetFooterMargin(PDF_MARGIN_FOOTER);
 
-            // Set auto page breaks
             $pdf->SetAutoPageBreak(true, PDF_MARGIN_BOTTOM);
 
-            // Set image scale factor
             $pdf->setImageScale(PDF_IMAGE_SCALE_RATIO);
 
-            // Add a page
             $pdf->AddPage();
 
-            // Generate HTML view
             $html = view('products.pdf', compact('products'))->render();
 
-            // Output HTML content
             $pdf->writeHTML($html, true, false, true, false, '');
 
-            // Close and output PDF document
             return response()->streamDownload(function() use ($pdf) {
                 $pdf->Output('products.pdf', 'I');
             }, 'products.pdf');
